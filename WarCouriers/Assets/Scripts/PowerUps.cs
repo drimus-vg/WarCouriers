bc1237caba37caa51b9d8373f0174176e2feb4ad
knochesin <lucaa-.dvt@hotmail.com>
2020-10-05 02:17:46 -0300

    xd

--- WarCouriers/Assets/Scripts/PowerUps.cs
+++ WarCouriers/Assets/Scripts/PowerUps.cs
@@ -10,6 +10,12 @@ public class PowerUps : MonoBehaviour
     public float slowmodeDuration = 3f;
 
 
+    void Update()
+    {
+        Time.timeScale += (1f / slowmodeDuration) * Time.unscaledDeltaTime;
+        Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
+        triggerbody.AddTorque(Vector3.up * 50);
+    }
 
 
     private void OnCollisionEnter(Collision other)
@@ -27,18 +33,9 @@ public class PowerUps : MonoBehaviour
             {
                 Time.timeScale = slowmodeFactor;
                 Time.fixedDeltaTime = Time.timeScale * .02f;
-                Debug.Log(Time.timeScale);
+                Debug.Log("hola");
             }
             Destroy(triggerbody.gameObject);
         }
     }
-
-    private void Update()
-    {
-        Time.timeScale += (1f / slowmodeDuration) * Time.unscaledDeltaTime;
-        Time.timeScale = Mathf.Clamp(Time.timeScale, 0f, 1f);
-        triggerbody.AddTorque(Vector3.up*50);
-
-    }
-
 }
