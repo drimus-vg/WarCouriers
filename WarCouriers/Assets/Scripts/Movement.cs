58a182ad83b81ba19641a86e9c3dcd1bf5cf1853
knochesin <lucaa-.dvt@hotmail.com>
2020-10-05 01:37:50 -0300

    jaja asi es

--- WarCouriers/Assets/Scripts/Movement.cs
+++ WarCouriers/Assets/Scripts/Movement.cs
@@ -5,41 +5,66 @@ public class Movement : MonoBehaviour
 
     public Rigidbody rb;
     public float Forward = 2000f;
-    public float Side = 500f;
-    public float Up = 200f;
     //[HideInInspector]
     public bool canMove;
+    public float Up = 100;
+    public float speed;
+
 
     private void Start()
     {
         canMove = true;
+        rb = GetComponent<Rigidbody>();
     }
+
 
     // Update is called once per frame
     void FixedUpdate()
-    {
+        {
+        float Side = Input.GetAxis("Horizontal");
+        bool a = Input.GetKey(KeyCode.A);
+        bool d = Input.GetKey(KeyCode.D);
+        bool space = Input.GetKey(KeyCode.Space);
+
+
+
+
         if (canMove)
-        {
-            rb.AddForce(0, 0, Forward * Time.deltaTime, ForceMode.VelocityChange);
-
-            if (Input.GetKey("d"))
             {
-                rb.AddForce(Side * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+
+            if (rb.velocity < new Vector3(0,0,15)
+            {
+                rb.AddForce(0, 0, Forward * Time.deltaTime, ForceMode.Impulse);
             }
 
-            if (Input.GetKey("a"))
+        else rb.AddForce(0, 0, 0);
+
+        if (a)
             {
-                rb.AddForce(-Side * Time.deltaTime, 0, 0,ForceMode.VelocityChange);
+                Vector3 asideVect = new Vector3(Side, 0, 0);
+                asideVect = asideVect.normalized * speed * Time.deltaTime;
+                rb.MovePosition(transform.position + asideVect);
             }
 
-            if (Input.GetKeyDown("space"))
+            if (d)
             {
-                rb.AddForce(0, Up, -Forward/2, ForceMode.VelocityChange);
+                Vector3 dsideVect = new Vector3(Side, 0, 0);
+                dsideVect = dsideVect.normalized * speed * Time.deltaTime;
+                rb.MovePosition(transform.position + dsideVect);
+            }
+
+            if (space)
+            {
+                rb.AddForce(0, Up * Time.deltaTime, -Forward * Time.deltaTime, ForceMode.Impulse);
+            }
+
+
+        }
+            else
+            {
+                Debug.Log("NO ME PUEDO MOVER AAA");
             }
         }
-        else
-        {
-            Debug.Log("NO ME PUEDO MOVER AAA");
-        }
-    }
 }
+
+
---
+++ WarCouriers/Assets/Scripts/SwordmanSpin.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordmanSpin : MonoBehaviour
+{
+    public Vector3 spin;
+    public Rigidbody swordmanRB;
+    bool entertrigger;
+
+
+    private void Start()
+    {
+        entertrigger = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            entertrigger = true;
+        }   
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            entertrigger = false;
+        }
+    }
+
+
+    void Update()
+    {
+        if (entertrigger)
+            swordmanRB.AddTorque(spin);
+        else swordmanRB.AddTorque(Vector3.zero);
+
+    }
+
+}
